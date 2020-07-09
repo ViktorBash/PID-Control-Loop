819b60acec0369ebc3fca1f759ecbf09a585edb1
Viktor Basharkevich <basharkevichv@gmail.com>
2020-07-09 13:46:33 -0400

    Created servo_control.py to move the servos that will be controlling the rocket movement

---
+++ servo_control.py
@@ -0,0 +1,30 @@
+"""
+Code to control the two servo motors which will move the rocket.
+"""
+
+from gpiozero import Servo
+from gpiozero.tools import sin_values
+import time
+from time import sleep
+
+# Setup servo 1
+servo1 = Servo(17)
+# Setup servo 2
+servo2 = Servo(19)
+
+
+def move_servo_1(angle):
+    servo1.angle = angle
+
+
+def move_servo_2(angle):
+    servo2.angle = angle
+
+
+# For testing purposes
+if __name__ == "__main__":
+    while True:
+        servo1.min()
+        sleep(1)
+        servo2.max()
+        sleep(1)
