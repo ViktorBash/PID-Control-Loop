76895faa191fb5805ee6eeed2ddb5306520999ab
Viktor Basharkevich <basharkevichv@gmail.com>
2020-07-09 15:00:19 -0400

    Documented data.py on how the CSV file system works

--- data.py
+++ data.py
@@ -1,12 +1,19 @@
 """
 Script to collect data in all stages of the flight.
-Data that will be collected:
+Data that will be collected will be put into a csv file:
     -Location
     -Altitude
     -
 
 Need to create an iterable object in order to record data in the CSV format
 We will be using a list as the iterable object
+
+First we read csv_number.txt and find the number.
+Then we write the number + 1 to the csv_number.txt file
+Then we create a CSV file with the number in the name and write whatever we want
+
+This system ensures that all the CSV files have names in ascending order so we
+can store data from each flight effortlessly.
 """
 
 import csv
