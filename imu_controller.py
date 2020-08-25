0c20bea5050c8ff9665182a79ea65e5149073939
ViktorB <basharkevichv@gmail.com>
2020-08-25 18:27:37 -0400

    Removed unnecessary code

--- imu_controller.py
+++ imu_controller.py
@@ -15,35 +15,35 @@ from Adafruit_BNO055 import BNO055
 bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
 # BeagleBone Black configuration with default I2C connection (SCL=P9_19, SDA=P9_20),
 # and RST connected to pin P9_12:
-#bno = BNO055.BNO055(rst='P9_12')
+# bno = BNO055.BNO055(rst='P9_12')
 
 
 # Enable verbose debug logging if -v is passed as a parameter.
-if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
-    logging.basicConfig(level=logging.DEBUG)
+# if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
+#     logging.basicConfig(level=logging.DEBUG)
 
 # Initialize the BNO055 and stop if something went wrong.
-if not bno.begin():
-    raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+# if not bno.begin():
+#     raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
 
 # Print system status and self test result.
-status, self_test, error = bno.get_system_status()
-print('System status: {0}'.format(status))
-print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
-# Print out an error if system status is in error mode.
-if status == 0x01:
-    print('System error: {0}'.format(error))
-    print('See datasheet section 4.3.59 for the meaning.')
-
-# Print BNO055 software revision and other diagnostic data.
-sw, bl, accel, mag, gyro = bno.get_revision()
-print('Software version:   {0}'.format(sw))
-print('Bootloader version: {0}'.format(bl))
-print('Accelerometer ID:   0x{0:02X}'.format(accel))
-print('Magnetometer ID:    0x{0:02X}'.format(mag))
-print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
-
-print('Reading BNO055 data, press Ctrl-C to quit...')
+# status, self_test, error = bno.get_system_status()
+# print('System status: {0}'.format(status))
+# print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
+# # Print out an error if system status is in error mode.
+# if status == 0x01:
+#     print('System error: {0}'.format(error))
+#     print('See datasheet section 4.3.59 for the meaning.')
+#
+# # Print BNO055 software revision and other diagnostic data.
+# sw, bl, accel, mag, gyro = bno.get_revision()
+# print('Software version:   {0}'.format(sw))
+# print('Bootloader version: {0}'.format(bl))
+# print('Accelerometer ID:   0x{0:02X}'.format(accel))
+# print('Magnetometer ID:    0x{0:02X}'.format(mag))
+# print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
+#
+# print('Reading BNO055 data, press Ctrl-C to quit...')
 while True:
     # Read the Euler angles for heading, roll, pitch (all in degrees).
     heading, roll, pitch = bno.read_euler()
