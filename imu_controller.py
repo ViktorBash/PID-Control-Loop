0175144edf83d4cea3f950dec1fc4745a7e7930c
ViktorB <basharkevichv@gmail.com>
2020-08-27 14:19:20 -0400

    Added logging and status

--- imu_controller.py
+++ imu_controller.py
@@ -3,16 +3,28 @@ Will control the BNO055 IMU made for the Raspberry Pi
 """
 
 
-from Adafruit_BNO055 import BNO055
-
-
 def run_imu_controller():
+    import sys
+    import logging
+    from Adafruit_BNO055 import BNO055
+
     # Raspberry Pi configuration with serial UART
     bno = BNO055.BNO055(serial_port='/dev/serial0', rst=18)
+
+    # Enable verbose debug logging if -v is passed as a parameter.
+    if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
+        logging.basicConfig(level=logging.DEBUG)
+
     if not bno.begin():
         raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
 
     status, self_test, error = bno.get_system_status()
+    print('System status: {0}'.format(status))
+    print('Self test result (0x0F is normal): 0x{0:02X}'.format(self_test))
+    # Print out an error if system status is in error mode.
+    if status == 0x01:
+        print('System error: {0}'.format(error))
+        print('See datasheet section 4.3.59 for the meaning.')
 
     # Read the Euler angles for heading, roll, pitch (all in degrees).
     heading, roll, pitch = bno.read_euler()
