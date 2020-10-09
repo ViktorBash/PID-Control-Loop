2995c2dce288e1980f653a93cd027c7b8b1a2f03
ViktorB <basharkevichv@gmail.com>
2020-10-09 17:01:17 -0400

    Changed Servo object to AngularServo

--- servo_control.py
+++ servo_control.py
@@ -2,15 +2,17 @@
 Two functions to control the two servo motors for the project
 """
 
-from gpiozero import Servo
+from gpiozero import Servo, AngularServo
 from gpiozero.tools import sin_values
 import time
 from time import sleep
 
 # Setup servo 1
-servo1 = Servo(12)
+# servo1 = Servo(12)
+servo1 = AngularServo(12, min_angle=-90, max_angle=90)
 # Setup servo 2
-servo2 = Servo(19)
+# servo2 = Servo(19)
+servo2 = AngularServo(19, min_angle=-90, max_angle=90)
 
 
 def move_servo_1(angle):
