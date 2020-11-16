b2a75fbf410e7b5ef16a6b1caf7f6037dbfe651f
ViktorB <basharkevichv@gmail.com>
2020-11-15 21:21:51 -0500

    Fixed typo on line 35 and 37 in servo_control_pwm.py

--- servo_control_pwm.py
+++ servo_control_pwm.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     pwm2.set_servo_pulsewidth(servo2, 500 + (1000/90 * 15))
 
     # Turning off the servo
-    pwm1.set_PWM_dutycyle(servo1, 0)
+    pwm1.set_PWM_dutycycle(servo1, 0)
     pwm1.set_PWM_frequency(servo1, 0)
-    pwm2.set_PWM_dutycyle(servo2, 0)
+    pwm2.set_PWM_dutycycle(servo2, 0)
     pwm2.set_PWM_frequency(servo2, 0)
