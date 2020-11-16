118e3d5dceb58b399b1b6588214b97151cfb9313
ViktorB <basharkevichv@gmail.com>
2020-11-15 21:39:38 -0500

    Changed degree movement to 1 and commented out turning off the servos

--- servo_control_pwm.py
+++ servo_control_pwm.py
@@ -23,8 +23,8 @@ if __name__ == "__main__":
     # 1500: 90 Degrees
     # 1 Degree Addition = 1000/90, aka: 11.1 recurring
     print("0 Degrees ")
-    pwm1.set_servo_pulsewidth(servo1, 500 + (1000/90 * 0.1))
-    pwm2.set_servo_pulsewidth(servo2, 500 + (1000/90 * 0.1))
+    pwm1.set_servo_pulsewidth(servo1, 500 + (1000/90 * 1))
+    pwm2.set_servo_pulsewidth(servo2, 500 + (1000/90 * 1))
     time.sleep(3)
 
     print("15 Degrees")
@@ -33,12 +33,12 @@ if __name__ == "__main__":
     time.sleep(3)
 
     print("0 Degrees ")
-    pwm1.set_servo_pulsewidth(servo1, 500 + (1000/90 * 0.1))
-    pwm2.set_servo_pulsewidth(servo2, 500 + (1000/90 * 0.1))
+    pwm1.set_servo_pulsewidth(servo1, 500 + (1000/90 * 1))
+    pwm2.set_servo_pulsewidth(servo2, 500 + (1000/90 * 1))
     time.sleep(3)
 
     # Turning off the servo
-    pwm1.set_PWM_dutycycle(servo1, 0)
-    pwm1.set_PWM_frequency(servo1, 0)
-    pwm2.set_PWM_dutycycle(servo2, 0)
-    pwm2.set_PWM_frequency(servo2, 0)
+    # pwm1.set_PWM_dutycycle(servo1, 0)
+    # pwm1.set_PWM_frequency(servo1, 0)
+    # pwm2.set_PWM_dutycycle(servo2, 0)
+    # pwm2.set_PWM_frequency(servo2, 0)
