d57816bb5545491d63cf846d0942deba06401930
ViktorB <basharkevichv@gmail.com>
2020-11-15 20:57:34 -0500

    Added new version for servo control that is PWM and should have less jitters

---
+++ servo_control_pwm.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import pigpio
+import time
+
+# Setting the servo pins
+servo1 = 12
+servo2 = 19
+
+# Creating servo 1
+pwm1 = pigpio.pi()
+pwm1.set_mode(servo1, pigpio.OUTPUT)
+pwm1.set_PWM_frequency(servo1, 50)
+
+
+# Creating servo 2
+pwm2 = pigpio.pi()
+pwm2.set_mode(servo2, pigpio.OUTPUT)
+pwm1.set_PWM_frequency(servo2, 50)
+
+# Testing area
+if __name__ == "__main__":
+    # 500: 0 Degrees
+    # 1500: 90 Degrees
+    # 1 Degree Addition = 1000/90, aka: 11.1 recurring
+    print("0 Degrees ")
+    pwm1.set_servo_pulsewidth(servo1, 500)
+    pwm2.set_servo_pulsewidth(servo2, 500)
+    time.sleep(3)
+
+    print("15 Degrees")
+    pwm1.set_servo_pulsewidth(servo1, 500 + (1000/90 * 15))
+    pwm2.set_servo_pulsewidth(servo2, 500 + (1000/90 * 15))
+
+    # Turning off the servo
+    pwm1.set_PWM_dutycyle(servo1, 0)
+    pwm1.set_PWM_frequency(servo1, 0)
+    pwm2.set_PWM_dutycyle(servo2, 0)
+    pwm2.set_PWM_frequency(servo2, 0)
