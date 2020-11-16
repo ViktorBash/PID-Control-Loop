58b902e4b063cabaa492dfd4659b5179b4edf337
ViktorB <basharkevichv@gmail.com>
2020-11-15 20:31:26 -0500

    Changed return of quaternion_to_euler from radians to degrees

--- master.py
+++ master.py
@@ -243,6 +243,9 @@ def quaternion_to_euler():
     t3 = +2.0 * (w * z + x * y)
     t4 = +1.0 - 2.0 * (y * y + z * z)
     heading = math.atan2(t3, t4)
+    roll = math.degrees(roll)
+    pitch = math.degrees(pitch)
+    heading = math.degrees(heading)
     return [heading, pitch, roll]
 
 
